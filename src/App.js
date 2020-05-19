21a4e8643251b8dddc85fdacad8063e4a5b8cce3
Anna <anna@iMac-Alexey.local>
2020-05-19 23:04:09 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Main from './screens/Main';
+import ProductList from './screens/ProductList';
+import AboutProduct from './screens/AboutProduct';
+import Basket from './screens/Basket';
+import Profile from './screens/Profile';
+import FriendShip from './screens/FriendShip';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import PreCacheImg from 'react-precache-img';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div style={{ backgroundColor: '#E5E5E5' }}>
+			<PreCacheImg
+      images={require('../src/assets/icons/slider.png')}
+    />
+			<Router>
+				<Route exact path="/" component={Main}/>
+				<Route exact path="/Catalog" component={ProductList}/>
+				<Route exact path="/About" component={AboutProduct}/>
+				<Route exact path="/Basket" component={Basket}/>
+				<Route exact path="/Profile" component={Profile}/>
+				<Route exact path="/FriendShip" component={FriendShip}/>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
